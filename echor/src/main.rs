a0c00d578b093f12df8de55c230e0893fc4910df
ben <ben@bnsmcx.com>
2022-04-14 15:20:07 +0700

    Finish main.rs

--- echor/src/main.rs
+++ echor/src/main.rs
@@ -1,3 +1,29 @@
+use clap::App;
+use clap::Arg;
+
 fn main() {
-    println!("Hello, world!");
+  let matches = App::new("echor")
+    .version("0.1.0")
+    .author("Bruce Wayne")
+    .about("Rusty echo")
+    .arg(
+      Arg::with_name("text")
+        .value_name("TEXT")
+        .help("Input text")
+        .required(true)
+        .min_values(1),
+    )
+    .arg(
+      Arg::with_name("omit_newline")
+        .short("n")
+        .help("Do not print newline")
+        .takes_value(false),
+    )
+    .get_matches();
+
+    let text = matches.values_of_lossy("text").unwrap();
+    let omit_newline = matches.is_present("omit_newline");
+    let ending = if omit_newline { "" } else { "\n" };
+
+  println!("{}{}", text.join(" "), ending);
 }
